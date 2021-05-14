4b5318b93dc6134c896ea9dc196f3ec74cc87560
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-05-15 00:51:59 +0800

    Update AdminController.java

--- olife-admin/src/main/java/com/cloud/olifeadmin/rest/AdminController.java
+++ olife-admin/src/main/java/com/cloud/olifeadmin/rest/AdminController.java
@@ -24,9 +24,8 @@ public class AdminController {
     MqService mqService;
 
     @RequestMapping("/mq/{email}")
-    public RestResponse<String> test(@PathVariable("email") String email){
-        String[] tos = {email};
-        ToEmail toEmail = new ToEmail(tos, "OLife注册即可安心养老", "app下载的网址:https://github.com/AbrahamTemple/O-Life");
+    public RestResponse<String> test(@PathVariable("email") String[] emails){
+        ToEmail toEmail = new ToEmail(emails, "OLife注册即可安心养老", "app下载的网址:https://github.com/AbrahamTemple/O-Life");
         return new RestResponse<String>(HttpStatus.OK.value(), HttpStatus.OK.toString(),mqService.SendEmail(toEmail));
     }
 }
